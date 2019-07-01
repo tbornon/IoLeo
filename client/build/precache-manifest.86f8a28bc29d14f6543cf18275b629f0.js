78b119eb3cdca75cfeeeccfcede51abe10e6a499
BORNON Théophile <theophile.bornon@protonmail.com>
2019-07-01 12:28:21 +0200

    new fuild

---
+++ client/build/precache-manifest.86f8a28bc29d14f6543cf18275b629f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42817adb1156a41f4373ad3f953669d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffc7e58f72531fd4ca37",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "4484b29f8b6a2eb4310c",
+    "url": "/static/js/2.5b08f973.chunk.js"
+  },
+  {
+    "revision": "ffc7e58f72531fd4ca37",
+    "url": "/static/js/main.a1a35630.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
