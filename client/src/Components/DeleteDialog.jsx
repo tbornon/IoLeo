e351e9447d9b7b7862ff7ae0a2f95978cc283882
BORNON Théophile <theophile.bornon@protonmail.com>
2019-07-08 13:50:49 +0200

    unused var

--- client/src/Components/DeleteDialog.jsx
+++ client/src/Components/DeleteDialog.jsx
@@ -4,13 +4,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import TextField from '@material-ui/core/TextField';
 
 import { SuccessSimple, DangerSimple } from "./Buttons";
 import { withSnackbar } from "./SnackbarProvider";
