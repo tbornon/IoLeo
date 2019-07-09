2d6f28c83610c35288dffeb2c7de406cd2bb3d39
BORNON Théophile <theophile.bornon@protonmail.com>
2019-07-09 11:26:31 +0200

    patch io connection broken

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-RoomRoutes(app);
+RoomRoutes(app, io);
 
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
