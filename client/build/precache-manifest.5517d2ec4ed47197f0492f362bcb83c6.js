5e7eb73847f19d4cba929a3255d6c7c827c7f360
BORNON Théophile <theophile.bornon@protonmail.com>
2019-07-09 11:32:41 +0200

    new build

---
+++ client/build/precache-manifest.5517d2ec4ed47197f0492f362bcb83c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a7c546d5f311f168a98d18f8a5d11be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15d12a5aaa43a5d96f8c",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "fc3dc16def8bbd1beb8f",
+    "url": "/static/js/2.4f1a7443.chunk.js"
+  },
+  {
+    "revision": "15d12a5aaa43a5d96f8c",
+    "url": "/static/js/main.71680d5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
